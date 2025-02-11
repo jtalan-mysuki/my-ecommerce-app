aa11f152150048b5538d8d33e4ac7cb9a280cb74
Albert John Tulop <albertjohntulop@Alberts-MacBook-Pro.local>
2025-02-11 16:07:11 +0800

    Apply stepper count functionality

--- components/Products/ProductItem.tsx
+++ components/Products/ProductItem.tsx
@@ -1,14 +1,23 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 
 import productItemStyles from './styles'
 
 const ProductItem = ({ item }) => {
   const { name, price, image_src, quantity } = item
+  const [count, setCount] = useState(quantity)
 
-  const renderStepperOption = ({text}) => {
+  const handleStepper = (action) => {
+    if (action === 'increment') {
+      setCount(count=> count + 1)
+    } else {
+      setCount(count => count  - 1)
+    }
+  }
+
+  const renderStepperOption = ({text, action}) => {
     return (
-      <View style={{
+      <TouchableOpacity onPress={() => handleStepper(action)} style={{
         borderWidth: .5, 
         borderColor: 'black', 
         borderRadius: 5,
@@ -17,7 +26,7 @@ const ProductItem = ({ item }) => {
         alignItems: 'center',
         }}>
         <Text>{text}</Text>
-      </View>
+      </TouchableOpacity>
     )
   }
 
@@ -49,9 +58,9 @@ const ProductItem = ({ item }) => {
         width: '30%', 
         justifyContent: 'space-around',
         }}>
-          {renderStepperOption({text: '-'})}
-          <Text>{quantity}</Text>
-          {renderStepperOption({text: '+'})}
+          {renderStepperOption({text: '-', action: 'decrement'})}
+          <Text>{count || 0}</Text>
+          {renderStepperOption({text: '+', action: 'increment'})}
       </View>
     )
   }
@@ -63,7 +72,7 @@ const ProductItem = ({ item }) => {
       {/* product info */}
       <View style={productItemStyles.productInfo}>
         {renderProductInfo()}
-        {renderProductCount()}
+        {renderProductCount(count)}
       </View>
     </View>
   )
